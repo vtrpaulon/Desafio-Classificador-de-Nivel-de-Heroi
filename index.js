055ee82cf99100fdd57996ec1bee685b6138436c
VitorPaulon <71847592+vtrpaulon@users.noreply.github.com>
2025-12-30 17:02:05 +0000

    Finalizando o processo

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //primeiro
+let nomeDoHeroi = "SuperPapel";
+let xp = 15000;
+
+if (xp <= 1000){
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ferro`)
+}else if (xp <= 2000){
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Bronze`)
+}else if (xp <= 5000){
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Prata`)
+}else if (xp <= 7000){
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ouro`)
+}else if (xp <= 8000){
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Platina`)
+}else if (xp <= 9000){
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Ascendente`)
+}else if (xp <= 10000){
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Imortal`)
+}else{
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de Radiante`)   
+}
